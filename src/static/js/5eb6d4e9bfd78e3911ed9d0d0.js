0dbb8cf20fae3a46e8d35689e3024f2dc0b3b4b7
shravankumar9892 <shravankumarshetty9892@gmail.com>
2019-05-20 21:47:36 +0530

    static frontend files added

---
+++ src/static/js/5eb6d4e9bfd78e3911ed9d0d0.js
@@ -0,0 +1,20 @@
+;(function(){
+
+  var l = document.createElement('link');
+  l.setAttribute('rel', 'stylesheet');
+  l.onload = function(){
+    document.body.insertAdjacentHTML('beforeend','<a href="https://splitting.js.org" target="_blank" class="splitting-mark splitting" title="Splitting.js Library" style="--word-total:1; --char-total:9;"><div class="brad-split"></div><span class="word" data-word="Splitting" style="--word-index:0;"><span class="char" data-char="S" style="--char-index:0;">S</span><span class="char" data-char="p" style="--char-index:1;">p</span><span class="char" data-char="l" style="--char-index:2;">l</span><span class="char" data-char="i" style="--char-index:3;">i</span><span class="char" data-char="t" style="--char-index:4;">t</span><span class="char" data-char="t" style="--char-index:5;">t</span><span class="char" data-char="i" style="--char-index:6;">i</span><span class="char" data-char="n" style="--char-index:7;">n</span><span class="char" data-char="g" style="--char-index:8;">g</span></span></a>');
+  }
+
+  l.setAttribute('href', 'https://codepen.io/shshaw/pen/5eb6d4e9bfd78e3911ed9d0d0dfce69b.css');
+  document.head.appendChild(l);
+
+})();
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-6412794-6', 'auto');
+ga('send', 'pageview');
